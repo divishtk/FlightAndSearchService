e227174fb24f65f2a19a2536829755df83825c56
divishtk <divisht.avion@gmail.com>
2023-09-12 14:54:18 +0530

    added body parser

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const bodyParser = require('body-parser')
+
 const {PORT} = require('./config/serverConfig')
 const serverStartUp = async () => {
   const app = express();
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended:true}))
   app.listen(PORT, () => {
     console.log(`server started at port ${PORT}`);
   });
